3d709f45de79b8ccd852c58ea8abe0f5a4511646
RobsonBSF <robsonbsfilho@gmail.com>
2023-11-15 11:31:12 -0300

    Criadas as rotas do projeto

---
+++ backend/routes.java
@@ -0,0 +1,41 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String index() {
+        return "../frontend/views/index.html";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "../frontend/views/register.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "../frontend/views/login.html";
+    }
+
+    @GetMapping("/quiz")
+    public String quiz() {
+        return "../frontend/views/quiz.html";
+    }
+
+    @GetMapping("/comunity")
+    public String comunity() {
+        return "../frontend/views/comunity.html";
+    }
+
+    @GetMapping("/solutions")
+    public String solutions() {
+        return "../frontend/views/solutions.html";
+    }
+
+    @GetMapping("/404")
+    public String page404() {
+        return "../frontend/views/404.html";
+    }
+}
